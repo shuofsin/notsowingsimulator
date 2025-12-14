9f6310c390b90bf6550e1d49de89d086652469e5
shuofsin <sinhah239@gmail.com>
2025-12-14 15:32:11 -0700

    Reworked some tiles, fixed y-sort

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 class_name Player
 
 var speed: float = 100.0
+@onready var collision_polygon_2d: CollisionPolygon2D = %CollisionPolygon2D
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+func _process(delta: float) -> void:
+	pass
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
