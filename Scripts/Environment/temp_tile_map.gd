55c9b25945f1eafe649ecd86b759483a28a76704
shuofsin <sinhah239@gmail.com>
2025-12-17 18:00:53 -0700

    Added random gen

--- Scripts/Environment/temp_tile_map.gd
+++ Scripts/Environment/temp_tile_map.gd
@@ -2,11 +2,16 @@ extends TileMapLayer
 
 
 func _ready() -> void: 
+	for i in randi_range(0, 40):
+		for j in randi_range(0, 40):
+			if randi_range(1, 5)%5 == 0: 
+				set_cell(Vector2i(i, j), 0, Vector2i(0, 0), 0)
+	
 	var block_scene: PackedScene = preload("res://Scenes/Environment/temp_block.tscn")
-	for pos in self.get_used_cells():
-		print(self.map_to_local(pos))
+	for pos in get_used_cells():
+		print(map_to_local(pos))
 		var new_block: Node2D = block_scene.instantiate()
-		new_block.global_position = self.map_to_local(pos)
+		new_block.global_position = map_to_local(pos)
 		get_parent().add_child.call_deferred(new_block)
 	visible = false
 	pass
