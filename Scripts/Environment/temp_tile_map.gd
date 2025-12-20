1a2746057a51aaa188bff4331568cda51fc70ac0
shuofsin <sinhah239@gmail.com>
2025-12-20 12:56:18 -0700

    Added variable wheat spawn density/size

--- Scripts/Environment/temp_tile_map.gd
+++ Scripts/Environment/temp_tile_map.gd
@@ -1,10 +1,12 @@
 extends TileMapLayer
 
+const WORLD_SIZE: int = 100
+const WORLD_DENSITY: int = 3
 
 func _ready() -> void: 
-	for i in randi_range(0, 40):
-		for j in randi_range(0, 40):
-			if randi_range(1, 5)%5 == 0: 
+	for i in randi_range(0, WORLD_SIZE):
+		for j in randi_range(0, WORLD_SIZE):
+			if randi_range(1, WORLD_DENSITY)%WORLD_DENSITY == 0: 
 				set_cell(Vector2i(i, j), 0, Vector2i(0, 0), 0)
 	
 	var block_scene: PackedScene = preload("res://Scenes/Environment/temp_block.tscn")
