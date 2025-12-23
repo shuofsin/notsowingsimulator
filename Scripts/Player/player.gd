c76c877c0aa163d0edac257b642029585064947c
shuofsin <sinhah239@gmail.com>
2025-12-22 18:30:49 -0700

    Added upgrades

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 class_name Player
 
-var speed: float = 50.0
-var TOTAL_SWING_TIME: float = 0.4
+
+var base_stats: Dictionary = {
+	"speed" : 50.0,
+	"swing_time" : 0.4,
+	"tool_scale" : 1.0
+}
+
+var speed: float = base_stats.speed
+var total_swing_time: float = base_stats.swing_time
 var swing_number: int = 0 
 var MAX_SWINGS: int = 3
 var is_swinging: bool = false
@@ -14,10 +21,22 @@ var SWING_RESET_TIME: float = 0.2
 
 func _ready() -> void: 
 	Global.player = self
+	speed = base_stats.speed
+	total_swing_time = base_stats.swing_time
+	hitbox_small.scale = Vector2(base_stats.tool_scale, base_stats.tool_scale)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("use") and !is_swinging:
 		_swing()
+	
+	if Input.is_action_just_pressed("1"):
+		speed *= 1.2
+	
+	if Input.is_action_just_pressed("2"):
+		total_swing_time *= 0.8
+	
+	if Input.is_action_just_pressed("3"):
+		hitbox_small.scale *= 1.2
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -44,9 +63,9 @@ func _swing() -> void:
 	swing_tween.finished.connect(_stop_swinging)
 	sounds.play()
 	if swing_number % 3 == 0: 
-		swing_tween.tween_property(hitbox_small, "rotation", hitbox_small.rotation + 2*PI, TOTAL_SWING_TIME * 1.5)
+		swing_tween.tween_property(hitbox_small, "rotation", hitbox_small.rotation + 2*PI, total_swing_time * 1.5)
 		return 
-	swing_tween.tween_property(hitbox_small, "rotation", hitbox_small.rotation + (PI/2 * (-1 if swing_number % 2 == 0 else 1)), TOTAL_SWING_TIME)
+	swing_tween.tween_property(hitbox_small, "rotation", hitbox_small.rotation + (PI/2 * (-1 if swing_number % 2 == 0 else 1)), total_swing_time)
 
 func _stop_swinging() -> void: 
 	hitbox_small.disable()
